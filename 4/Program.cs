80a1ccf20cbfd94590584fb0eb8e75528f1c6858
ThusSpokeLandau <lefthpavel@gmail.com>
2022-05-26 20:30:06 +0300

    task4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using Task4;
+
+public static class Program{
+    public static void Main(string [] args){
+        vectorTask();
+    }
+
+    private static void vectorTask(){
+       Vector vector = new Vector(new int[]{3,2,55,4,2});
+       try{
+           vector.quickSort(2,11);
+       }catch(ArgumentException e){
+           Console.WriteLine(e.Message);
+       }
+       vector.quickSort(0, 4);
+       
+       Console.WriteLine(vector);
+    }
+}
